b30c9a440f4a626170cdb2d8452f5f34790b6807
Eldritch Cheese <eldritch.cheese@gmail.com>
2015-03-29 10:21:24 -0400

    Reverted to tree with nodes as separate instances.
    So, it turns out that the compact form was slower,
      especially with epsilon=0.
    Factor of 5 slower or so.

--- include/KDTree.hh
+++ include/KDTree.hh
@@ -6,10 +6,15 @@
 #include <cfloat>
 #include <cmath> // for std::abs
 #include <cstddef> // for size_t
-#include <iostream>
 #include <memory> // for std::shared_ptr
 #include <tuple>
 #include <vector>
+
+template<typename T>
+class LeafNode;
+
+template<typename T>
+class InternalNode;
 
 template<typename T>
 double distance2(T a, T b){
@@ -20,147 +25,202 @@ double distance2(T a, T b){
 	return output;
 }
 
-struct Node{
-	size_t i_start, n;
-	char dimension;
-	double median;
-	size_t unused;
-	size_t left, right;
+template<typename T>
+class NodeBase{
+public:
+	typedef std::tuple<double, LeafNode<T>*, size_t> SearchRes;
+
+	NodeBase() : parent(nullptr) {}
+
+	// Gets the number of leaves that are direct or indirect children.
+	virtual int GetNumLeaves() = 0;
+
+	// Pops the closest value from the tree.
+	T PopClosest(T query, double epsilon){
+		auto res = GetClosestNode(query, epsilon);
+		NodeBase<T>* node_ptr = std::get<1>(res);
+		while(true){
+			node_ptr->ReduceLeaves();
+			node_ptr = node_ptr->parent;
+			if(node_ptr == nullptr){
+				break;
+			}
+		}
+		return std::get<1>(res)->PopValue(std::get<2>(res));
+	}
+
+	// Returns the closest value from the tree.
+	T GetClosest(T query, double epsilon){
+		auto res = GetClosestNode(query, epsilon);
+		return std::get<1>(res)->GetValue();
+	}
+	// Mark one leaf as having been finished.
+	virtual void ReduceLeaves() = 0;
+
+private:
+	// Returns a (distance,leafnode) pair of the closest value.
+	virtual SearchRes GetClosestNode(T query, double epsilon) = 0;
+	void SetParent(NodeBase<T>* par){parent = par;}
+	friend class InternalNode<T>;
+
+	NodeBase<T>* parent;
 };
 
-struct SearchRes{
-	SearchRes(size_t index, double dist2)
-		: index(index), dist2(dist2) { }
-	size_t index;
-	double dist2;
+template<typename T>
+class LeafNode : public NodeBase<T> {
+public:
+	LeafNode(std::vector<T> p_values)
+		: values(std::move(p_values)), used(values.size(),false), leaves_unused(values.size()) {
+		assert(leaves_unused>0);
+	}
+
+	virtual int GetNumLeaves(){
+		return leaves_unused;
+	}
+	virtual void ReduceLeaves(){
+		leaves_unused--;
+	}
+	void MarkAsUsed(size_t index){
+		used[index] = true;
+	}
+
+	T GetValue(size_t index){
+		return values[index];
+	}
+
+	T PopValue(size_t index){
+		used[index] = true;
+		return values[index];
+	}
+
+private:
+	virtual typename NodeBase<T>::SearchRes GetClosestNode(T query, double /* epsilon */){
+		assert(leaves_unused > 0);
+
+		double best_distance2 = DBL_MAX;
+		size_t best_index;
+		for(size_t i=0; i<values.size(); i++){
+			if(!used[i]){
+				double dist2 = distance2(values[i],query);
+				if(dist2 < best_distance2){
+					best_distance2 = dist2;
+					best_index = i;
+				}
+			}
+		}
+		return std::make_tuple(best_distance2, this, best_index);
+	}
+
+	std::vector<T> values;
+	std::vector<bool> used;
+	int leaves_unused;
+};
+
+template<typename T>
+class InternalNode : public NodeBase<T>{
+public:
+	InternalNode(std::unique_ptr<NodeBase<T> > p_left, std::unique_ptr<NodeBase<T> > p_right,
+							 int dimension, double median)
+		: left(std::move(p_left)), right(std::move(p_right)), dimension(dimension), median(median) {
+		num_leaves = left->GetNumLeaves() + right->GetNumLeaves();
+		left->SetParent(this);
+		right->SetParent(this);
+	}
+
+	virtual int GetNumLeaves(){
+		return num_leaves;
+	}
+	virtual void ReduceLeaves(){
+		num_leaves--;
+	}
+private:
+	virtual typename NodeBase<T>::SearchRes GetClosestNode(T query, double epsilon){
+		assert(num_leaves > 0);
+
+		// If one of the branches is empty, this becomes really easy.
+		if(left->GetNumLeaves() == 0){
+			return right->GetClosestNode(query, epsilon);
+		} else if (right->GetNumLeaves() == 0){
+			return left->GetClosestNode(query, epsilon);
+		}
+
+		// Check on the side that is recommended by the median heuristic.
+		double diff = query.get(dimension) - median;
+		auto res1 = (diff<0) ? left->GetClosestNode(query, epsilon) : right->GetClosestNode(query, epsilon);
+		if(diff*diff*(1+epsilon)*(1+epsilon) > std::get<0>(res1) ){
+			return res1;
+		}
+
+		// Couldn't bail out early, so check on the other side and compare.
+		auto res2 = (diff<0) ? right->GetClosestNode(query, epsilon) : left->GetClosestNode(query, epsilon);
+		return (std::get<0>(res1) < std::get<0>(res2)) ? res1 : res2;
+	}
+
+	std::unique_ptr<NodeBase<T> > left;
+	std::unique_ptr<NodeBase<T> > right;
+	int num_leaves;
+	int dimension;
+	double median;
 };
 
 template<typename T>
 class KDTree{
 public:
-	KDTree(std::vector<T> vec)
-		: search_points(std::move(vec)) {
-		nodes.reserve(2*search_points.size());
-		make_node(0, search_points.size());
+	KDTree(std::vector<T> vec){
+		root = make_node(vec.data(), vec.size());
 	}
 
 	T PopClosest(T query, double epsilon = 0){
-		auto res = GetClosestIndex(query, epsilon);
-
-		// Mark the result as having been used.
-		size_t node_index = 0;
-		nodes[0].unused--;
-		while(nodes[node_index].n != 1 &&
-			  nodes[node_index].dimension != -1) {
-			node_index = (res.index < nodes[nodes[node_index].right].i_start)
-				? nodes[node_index].left
-				: nodes[node_index].right;
-
-			nodes[node_index].unused--;
-		}
-
-		return search_points[res.index];
+		return root->PopClosest(query, epsilon);
 	}
 
 	T GetClosest(T query, double epsilon = 0){
-		auto res = GetClosestIndex(query, epsilon);
-		return search_points[res.index];
+		return root->GetClosest(query, epsilon);
 	}
 
 private:
-	SearchRes GetClosestIndex(T query, double epsilon, int node_index = 0){
-		Node& node = nodes[node_index];
-
-		// End condition, I am at a leaf node.
-		if(node.n == 1 ||
-		   node.dimension == -1){
-			int result_index = node.i_start;
-			double dist2 = distance2(search_points[result_index], query);
-			return {result_index, dist2};
-		}
-
-		// If one of the branches is empty, use the other one.
-		if(nodes[node.left].unused == 0){
-			return GetClosestIndex(query, epsilon, node.right);
-		} else if(nodes[node.right].unused == 0){
-			return GetClosestIndex(query, epsilon, node.left);
-		}
-
-		// Check on the side that is recommended by the median.
-		double diff = query.get(node.dimension) - node.median;
-		auto res1 = (diff<0) ?
-			GetClosestIndex(query, epsilon, node.left) :
-			GetClosestIndex(query, epsilon, node.right);
-
-		double allowed_diff = diff*(1+epsilon);
-		if(res1.dist2 < allowed_diff * allowed_diff){
-			return res1;
-		}
-
-		auto res2 = (diff<0) ?
-			GetClosestIndex(query, epsilon, node.right) :
-			GetClosestIndex(query, epsilon, node.left);
-
-		return (res1.dist2 < res2.dist2) ? res1 : res2;
-	}
-
-	size_t make_node(size_t i_start, size_t n, int start_dim = 0){
+	std::unique_ptr<NodeBase<T> > make_node(T* arr, size_t n, int start_dim = 0){
 		assert(n>0);
-		assert(i_start >= 0);
-		assert(i_start + n <= search_points.size());
-
-
-		size_t node_index = nodes.size();
-		nodes.emplace_back();
-		Node& node = nodes[node_index];
-
-		node.i_start = i_start;
-		node.n = n;
-		node.unused = n;
-
-		if(n == 1){
-			return node_index;
+		if(n < 50){
+			std::vector<T> elements = {arr,arr+n};
+			assert(elements.size() > 0);
+			return std::unique_ptr<LeafNode<T> >(new LeafNode<T>(elements));
 		} else {
 			// Loop over each dimension in case all values are equal in one dimension.
 			for(int dim_mod = 0; dim_mod<T::dimensions; dim_mod++){
 				int dimension = (start_dim + dim_mod) % T::dimensions;
 
 				// Find the median value.
-				auto region_start = search_points.begin() + i_start;
-				std::nth_element(region_start, region_start + n/2, region_start + n,
+				std::nth_element(arr, arr+n/2, arr+n,
 								 [dimension](T a, T b){return a.get(dimension) < b.get(dimension);});
-				double median_value = (region_start + n/2)->get(dimension);
+				double median_value = arr[n/2].get(dimension);
 
 				// Find the median index
-				auto median = std::partition(region_start, region_start + n,
-											 [dimension, median_value](T a){
-												 return a.get(dimension) < median_value;
-											 });
-				size_t median_index = median - region_start;
+				T* median = std::partition(arr, arr+n,
+										   [dimension, median_value](T a){
+											   return a.get(dimension) < median_value;
+										   });
+				size_t median_index = median - arr;
 
 				// Will be true so long as the coordinate is not equal for everything in this dimension.
 				if(median_index != 0 && median_index != n){
-					node.dimension = dimension;
-					node.median = (region_start + median_index)->get(dimension);
-
 					int next_dim = (dimension+1) % T::dimensions;
-
-					int left_index = make_node(i_start, median_index, next_dim);
-					node.left = left_index;
-					int right_index = make_node(i_start + median_index, n - median_index, next_dim);
-					node.right = right_index;
-					return node_index;
+					double median = arr[median_index].get(dimension);
+					return std::unique_ptr<InternalNode<T> >(new InternalNode<T>(
+												 make_node(arr, median_index, next_dim),
+												 make_node(arr+median_index,n-median_index, next_dim),
+												 dimension,median));
 				}
 			}
 
-			// If we got here, then everything remaining is equal.
-			node.dimension = -1;
-			return node_index;
+			// If we got here, then everything value remaining is equal.
+			std::vector<T> elements(arr,arr+n);
+			assert(elements.size() > 0);
+			return std::unique_ptr<LeafNode<T> >(new LeafNode<T>(elements));
 		}
 	}
 
-	std::vector<Node> nodes;
-	std::vector<T> search_points;
+	std::unique_ptr<NodeBase<T> > root;
 };
 
 #endif /* _KDTREE_H_ */
